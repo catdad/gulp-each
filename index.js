4d15f203bb1b3c23a3867f6a730bbe18d766becb
Kiril Vatev <vatev.1@gmail.com>
2015-08-31 22:52:11 -0400

    adding Stream handling

--- index.js
+++ index.js
@@ -13,12 +13,13 @@ function each(iterator, context) {
         var filepath = file.path || file.history[0];
         var name = path.basename(filepath);
         
-        var func = context ? iterator.bind(context) : iterator;
+        var iteratorFunc = context ? iterator.bind(context) : iterator;
         var content;
         
         var cb = function iteratorCallback(err, newContent) {
             if (file.isStream()) {
-            
+                file.contents = es.readArray([newContent]);
+                callback(null, file);
             } else {
                 file.contents = new Buffer(newContent);
                 callback(null, file);
@@ -26,10 +27,12 @@ function each(iterator, context) {
         };
         
         if (file.isStream()) {
-            
+            file.contents.pipe(es.wait(function(err, data) {
+                iteratorFunc(data, file, cb);
+            }));
         } else if (file.isBuffer()) {
             content = file.contents.toString('utf8');
-            iterator(content, file, cb);
+            iteratorFunc(content, file, cb);
         } else {
             // not sure what else it could be, but just deal with it
             callback(null, file);
--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-/* jshint node: true */
+/* jshint node: true, expr: true */
 /* global describe, it, beforeEach */
 
 var mocha = require('mocha');
@@ -59,5 +59,56 @@ describe('Buffers', function() {
 });
 
 describe('Streams', function() {
+    var each, input;
     
+    beforeEach(function() {
+        each = require('./index.js');
+        input = function() {
+            return new File({
+                contents: es.readArray([fakeData]),
+                path: 'file.ext',
+                base: __dirname
+            });
+        };
+    });
+    
+    it('takes a stream as a source', function(done) {
+        var source = each(function(content, file, cb) {
+            expect(content).to.equal(fakeData);
+            cb(null, content);
+            
+            done();
+        });
+        
+        source.write(input());
+        source.end();
+    });
+});
+
+describe('general', function() {
+    var each, input, obj = {};
+    
+    beforeEach(function() {
+        each = require('./index.js');
+        input = function() {
+            return new File({
+                contents: new Buffer(fakeData), //es.readArray([fakeData]),
+                path: 'file.ext',
+                base: __dirname
+            });
+        };
+    });
+    
+    it('can be called with a `this` arguments', function(done) {
+        var source = each(function(content, file, cb) {
+            expect(this).to.not.be.undefined;
+            expect(this).to.equal(obj);
+            
+            cb(null, content);
+            done();
+        }, obj);
+        
+        source.write(input());
+        source.end();
+    });
 });
