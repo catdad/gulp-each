d0886e1135f01cefa4644a9e737b63add20d077c
Kiril Vatev <vatev.1@gmail.com>
2017-02-08 22:14:46 -0500

    removing more dead code

--- index.js
+++ index.js
@@ -1,21 +1,11 @@
 /* jshint node: true */
 
 var path = require('path');
-var es = require('event-stream');
+
 var readFiles = require('read-vinyl-file-stream');
 
 var encodings = ['utf8', 'utf-8', 'buffer'];
 var defaultEnc = 'utf8';
-
-function castData(data, enc) {
-    var isBuffer = Buffer.isBuffer(data);
-
-    if (enc === 'buffer') {
-        return isBuffer ? data : (new Buffer(data));
-    } else {
-        return isBuffer ? data.toString(enc) : data;
-    }
-}
 
 function each(iterator, enc, context) {
     if (enc && typeof enc === 'string') {
